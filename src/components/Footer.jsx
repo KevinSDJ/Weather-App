c62d185c2e5e0575ffd942b24b41ea9d83b05ca3
KevinSDJ <sebadjkevin@gmail.com>
2021-12-19 22:26:07 -0300

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ import './Footer.css';
 function Footer(){
 	return(
 		<div>
-			<footer  id="footer"><span >Created by:<a style={{color:"white"}} href="https://github.com/KevinSDJ/FT-M2/tree/master/09-React-Routing/homework">@KevinSDJ</a></span></footer>
+			<footer  id="footer"><span >Created by:<a style={{color:"white"}} href="https://github.com/KevinSDJ/Weather-App">@KevinSDJ</a></span></footer>
 		</div>)
 }
 
